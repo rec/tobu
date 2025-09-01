1164137ac53aa25a7a20ef7252b14c7f12fa6787
Tom Ritchford <tom@swirly.com>
2025-09-01 22:40:21 +0200

    Add clone command

--- tobu/__main__.py
+++ tobu/__main__.py
@@ -1,10 +1,28 @@
+from functools import partial
 from typer import Argument, Option, Typer
 from pathlib import Path
 import os
+import atexit
 
-STATE = object()
+class State:
+    pass
 
-app = typer.Typer(
+
+STATE = State()
+BASH_COMMANDS = []
+
+atexit.register(lambda: print(*BASH_COMMANDS, sep='\n'))
+
+
+def bash(text):
+    BASH_COMMANDS.extend(t for s in text.splitlines() if (t := s.strip()))
+
+
+def cd_pytorch():
+    bash(f"cd {STATE.root_directory}/pytorch")
+
+
+app = Typer(
     no_args_is_help=True,
     context_settings={'help_option_names': ['-h', '--help']},
 )
@@ -15,11 +33,8 @@ def _root_directory():
     return path.parent if path.name == 'pytorch' else path
 
 
-@app.callback
-def callback(
-    all: bool = Option(
-        False, '--all', '-a', help="Update all of the repository, not just pytorch"
-    ),
+@app.callback()
+def tobu(
     dry_run: bool = Option(
         True, '--dry-run', '-d', help='Just print the bash commands, do not execute'
     ),
@@ -39,27 +54,46 @@ def callback(
 
 @app.command(help='Clone the repos')
 def clone(
+    all_: bool = Option(
+        False, '--all', '-a', name="_all", help="Update all of the repository, not just pytorch"
+    ),
     ref: str = Argument(
-        'upstream/viable/strict', '--ref', '-r', help='The ref ID to use'
+        'upstream/viable/strict', help='The ref ID to use'
     ),
     fetch: bool = Option(
         False, '--fetch', '-f', help='Run git fetch on upstream before starting',
     ),
     libraries: str = Option(
-        'torchbenchmark,torch-data,torch-data,torch-vision',
+        'torchbenchmark,torch-audio,torch-data,torch-vision',
         '--libraries', '-l', help="A list of libraries to clone parallel to pytorch",
+    ),
 ):
-    def clone():
-        STATE.root_directory.mkdir(exist_ok=True, parents=True)
-        yield f"cd {STATE.root_directory}"
-        yield f"git clone git@github.com:{STATE.user} pytorch"
+    if STATE.user == "pytorch":
+        add_remote = ""
+    else:
+        add_remote = "git remote add upstream git@github.com:pytorch/pytorch.git"
 
+    bash(f"""
+        mkdir -p {STATE.root_directory}
+        cd {STATE.root_directory}
+        git clone git@github.com:${STATE.user}/pytorch.git
+        cd pytorch
+        git submodule update --init --recursive
+        {add_remote}
+        cd {STATE.root_directory}"""
+    )
+    if all_:
+        for lib in libraries.split(","):
+            bash(f"git clone git@github.com:pytorch/{lib}.git")
 
 
 @app.command(help='')
 def update(
+    all_: bool = Option(
+        False, '--all', '-a', name="_all", help="Update all of the repository, not just pytorch"
+    ),
     ref: str = Argument(
-        'TODO', '--ref', '-r', help='The ref ID to use'
+        'upstream/viable/strict', help='The ref ID to use'
     ),
 ):
     pass
